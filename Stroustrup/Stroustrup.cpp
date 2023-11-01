53f972ae76af53a455178500aad17c2b64dae489
harikeshi <sshchegolikhin@gmail.com>
2023-11-02 01:06:46 +0300

    from pc

--- Stroustrup/Stroustrup.cpp
+++ Stroustrup/Stroustrup.cpp
@@ -30,7 +30,7 @@ void test() {
 	}
 	catch (some_error) {
 		cout << -1 << endl;
-	
+
 	}
 }
 
@@ -39,12 +39,63 @@ void test() {
 #include "Exceptions/FinalAction.cpp"
 #include "Exceptions/vector.cpp"
 
+#include <string>
+void ya(string s)
+{
+	// 2+2
+	// 11-22-33
+	// 2023
+
+	//string s;
+	//cin >> s;
+	double res = 0;
+	size_t i = 0;
+	string temp;
+
+	if (isdigit(s[0]))
+	{
+		while (i < s.size() && isdigit(s[i]))
+		{
+			temp += s[i];
+			i++;
+		}
+		res += stod(temp);
+		--i;
+	}
+
+	for (; i < s.size() - 1; ++i)
+	{
+		// 11-22-33
+		if (s[i] == '-' || s[i] == '+')
+		{
+			temp = "";
+			temp += s[i++];
+
+			while (i < s.size() && isdigit(s[i]))
+			{
+				temp += s[i];
+				++i;
+			}
+
+			res += stod(temp);
+
+			if (i < s.size() - 1)
+				--i;
+		}
+	}
+
+	cout << res;
+}
+
+
+
+
 int main()
 {
 	test();
-	File_ptr p("C:\\Users\\sshch\\Source\\Repos\\Harikeshi\\Stroustrup\\Stroustrup\\test.txt","r");
+	File_ptr p("C:\\Users\\sshch\\Source\\Repos\\Harikeshi\\Stroustrup\\Stroustrup\\test.txt", "r");
 	test1();
 	//vector<int> v(10,12);
-	cout << "end";
+
 }
 
